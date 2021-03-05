d7daf6f7d8c222aaaaf510b7d2db476c0f89af9d
Jamille Bacelar <70146570+Jamillebacelar@users.noreply.github.com>
2021-03-05 12:25:45 -0300

    versão inicial do projeto

---
+++ src/test/java/br/com/jamile/quitanda/QuitandaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jamile.quitanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuitandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
